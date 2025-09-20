f54fab6397b6933859260d448896a35a7ed7299d
Yash-1-tech <131190957+Yash-1-tech@users.noreply.github.com>
2025-09-20 17:10:34 +0530

    Update convert.py

--- convert.py
+++ convert.py
@@ -7,25 +7,26 @@ def main():
   elif scale == 'fahrenheit':
     convert_fah(value)
   elif scale == 'kelvin':
-    convert_kel(value)
+    a,b,c = convert_kel(value)
+    print_deg(a,b,c)
 
 
 def convert_cel(C):
   F = C*9/5 + 32
   K = C + 273.15
-  print_deg(F, C, K)
+  return F, C, K
 
 
 def convert_fah(F):
   C = (F - 32)*5/9
   K = C + 273.15
-  print_deg(F, C, K)
+  return F, C, K
 
 
 def convert_kel(K):
   C = K - 273.15
   F = C*9/5 + 32
-  print_deg(F,C,K)
+  return F,C,K
 
 def print_deg(a,b,c):
   print(f'°F = {a} , °C = {b}, °K = {c}')
