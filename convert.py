6bf2fee0d7c87f1f0b664ac9686d9afde65388d2
Yash-1-tech <131190957+Yash-1-tech@users.noreply.github.com>
2025-09-20 11:31:47 +0530

    Update convert.py

--- convert.py
+++ convert.py
@@ -1,9 +1,7 @@
-#°F = °C × 9/5 + 32
-#K=°C+273.15
 
 def main():
-  scale = input("Celsius , fahrenheit or Kelvin").lower().strip()
-  value = float(input('enter the temp in digit')
+  scale = input("Celsius , fahrenheit or Kelvin: ").lower().strip()
+  value = float(input('enter the temp in digit: '))
   if scale == 'celsius':
     convert_cel(value)
   elif scale == 'fahrenheit':
@@ -31,3 +29,6 @@ def convert_kel(K):
 
 def print_deg(a,b,c):
   print(f'°F = {a} , °C = {b}, °K = {c}')
+
+
+main()
